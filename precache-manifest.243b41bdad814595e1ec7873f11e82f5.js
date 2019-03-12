f4270d490b90c0f5e13ba84134ebf6fa89136529
Wejdi <wejdigharbi@yahoo.fr>
2019-03-12 11:42:29 +0100

    Updates

---
+++ precache-manifest.243b41bdad814595e1ec7873f11e82f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "334ecb72fcfe36fa213d",
+    "url": "/static/js/main.334ecb72.chunk.js"
+  },
+  {
+    "revision": "a852b0ebc3c774f96352",
+    "url": "/static/js/1.a852b0eb.chunk.js"
+  },
+  {
+    "revision": "334ecb72fcfe36fa213d",
+    "url": "/static/css/main.d67cc392.chunk.css"
+  },
+  {
+    "revision": "f4dbe3d0bdda52552312a04c852516a2",
+    "url": "/index.html"
+  }
+];
