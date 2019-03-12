3700b61bb414b2358a3aaad45d4a491afcfcf791
Wejdi <wejdigharbi@yahoo.fr>
2019-03-12 15:24:00 +0100

    Updates

---
+++ precache-manifest.331da493e3146427a0a25240afeb9618.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "445cae574ae28d4061a4",
+    "url": "/navbar-react/static/js/runtime~main.445cae57.js"
+  },
+  {
+    "revision": "e8c444c14f7a94e2bb31",
+    "url": "/navbar-react/static/js/main.e8c444c1.chunk.js"
+  },
+  {
+    "revision": "a852b0ebc3c774f96352",
+    "url": "/navbar-react/static/js/1.a852b0eb.chunk.js"
+  },
+  {
+    "revision": "e8c444c14f7a94e2bb31",
+    "url": "/navbar-react/static/css/main.a69f8e86.chunk.css"
+  },
+  {
+    "revision": "211710b369273432490864394eb577fa",
+    "url": "/navbar-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ab9c9ebd862c7a102365027641a1362b.js"
+  "/navbar-react/precache-manifest.331da493e3146427a0a25240afeb9618.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/navbar-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
